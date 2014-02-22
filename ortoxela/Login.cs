b03ddfea756d00532816d5674ed004b7fa3f2364
jaguilargt <jaguilar@nexogy.com>
2014-02-22 03:44:10 -0600

    Configuración de Guatemala

--- ortoxela/Login.cs
+++ ortoxela/Login.cs
@@ -47,7 +47,7 @@ namespace ortoxela
                     dt_rol = logica.Tabla(cadena);
                     clases.ClassVariables.id_rol = dt_rol.Rows[0][0].ToString();
 
-                    cadena = " SELECT Sucursal  FROM master.sucursales  WHERE IDSuc=1 ";
+                    cadena = " SELECT Sucursal  FROM master.sucursales  WHERE IDSuc=2 ";
                     DataTable dt_empresa = new DataTable();
                     dt_empresa = logica.Tabla(cadena);
                     clases.ClassVariables.nombreEmpresa = dt_empresa.Rows[0][0].ToString();
